513c00e5b7bc5ba15f468e69c13e5eee178b1fec
Rebecca Weinberger <rew@mit.edu>
2018-07-16 16:24:30 -0400

    Simple form for adding new pin

--- routes/index.js
+++ routes/index.js
@@ -13,13 +13,30 @@ router.get('/', function(req, res, next) {
     .then(_pins => pins = _pins)
     .then(() => userCollection.find().toArray())
     .then(_users => users = _users)
-    // .then(() => console.log(users))
     .then(() => res.render('index', { title: 'Map',  users: users, pins: pins }))
     .catch(e => console.error(e))
 });
 
-router.get('/example', function(req, res, next) {
-  res.send('hallo');
+router.post('/new_pin', function(req, res, next) {
+  const pin = {
+    userId: req.body.userId,
+    text: req.body.text,
+    location: req.body.location
+  }
+  addNewPin(pin, res);
 });
+
+const addNewPin = (pin, res) => {
+  const db = database.get();
+  const userCollection = db.collection('users');
+  const pinCollection = db.collection('pins');
+  let id;
+
+  pinCollection.insertOne(pin)
+    .then(data => id = data.ops._id)
+    .then(() => userCollection.findOneAndUpdate({userId: pin.userId}, {$push: {pins: id}}))
+    .then(() => res.redirect('/'))
+    .catch(e => console.error(e))
+}
 
 module.exports = router;
